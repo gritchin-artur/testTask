b7fa21ede04ea170cf6e4ab635d539b801a38b31
artur <123599698+gritchin-artur@users.noreply.github.com>
2024-03-29 22:13:27 +0100

    Update fetch.js

--- src/fetch/fetch.js
+++ src/fetch/fetch.js
@@ -26,31 +26,36 @@
 
 import axios from "axios";
 
-const soapRequest = `<?xml version="1.0" encoding="UTF-8"?>
-<env:Envelope 
-  xmlns:env="http://www.w3.org/2003/05/soap-envelope" 
-  xmlns:ns1="urn:ICUTech.Intf-IICUTech" 
-  xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-  xmlns:enc="http://www.w3.org/2003/05/soap-encoding">
-  <env:Body>
-    <ns1:Login env:encodingStyle="http://www.w3.org/2003/05/soap-encoding">
-      <UserName xsi:type="xsd:string">artur1@mail.com</UserName>
-      <Password xsi:type="xsd:string">artur1</Password>
-      <IPs xsi:type="xsd:string"></IPs>
-    </ns1:Login>
-  </env:Body>
-</env:Envelope>`;
+axios.defaults.baseURL =
+  "http://isapi.icu-tech.com/icutech-test.dll?mode=default";
 
+// Функция для формирования SOAP-сообщения
+function createSoapMessage(data) {
+  return `
+    <env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="urn:ICUTech.Intf-IICUTech" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:enc="http://www.w3.org/2003/05/soap-encoding">
+      <env:Body>
+        <ns1:Login env:encodingStyle="http://www.w3.org/2003/05/soap-encoding">
+          <UserName xsi:type="xsd:string">${data.username}</UserName>
+          <Password xsi:type="xsd:string">${data.password}</Password>
+          <IPs xsi:type="xsd:string"></IPs>
+        </ns1:Login>
+      </env:Body>
+    </env:Envelope>
+  `;
+}
+
+// Функция для отправки запроса на сервер
 export async function logInPost(data) {
-  const response = axios.post(
-    "http://isapi.icu-tech.com/icutech-test.dll?mode=default",
-    soapRequest,
-    {
+  const soapMessage = createSoapMessage(data);
+  try {
+    const response = await axios.post("", soapMessage, {
       headers: {
-        "Content-Type": "application/soap+xml",
+        "Content-Type": "application/xml",
       },
-    }
-  );
-  return response.data;
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error sending SOAP request:", error);
+    throw error;
+  }
 }
